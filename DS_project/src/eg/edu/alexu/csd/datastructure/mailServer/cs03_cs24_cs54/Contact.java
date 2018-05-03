01e211185be56a6bda2ae07fcbf0d900d044cd57
abdelrahman <abdelrahman@LAPTOP-BKLCQOBM>
2018-05-03 13:20:22 +0200

    dd

---
+++ DS_project/src/eg/edu/alexu/csd/datastructure/mailServer/cs03_cs24_cs54/Contact.java
@@ -0,0 +1,20 @@
+package eg.edu.alexu.csd.datastructure.mailServer.cs03_cs24_cs54;
+
+import eg.edu.alexu.csd.datastructure.mailServer.IContact;
+import java.io.File;
+import java.io.IOException;
+
+public class Contact implements IContact{
+
+	@Override
+	public boolean check(String email) {
+		// TODO Auto-generated method stub
+		String path = "D:\\Users\\mails.txt";
+		File 
+		
+		return false;
+	}
+	
+	
+
+}
