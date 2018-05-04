8d942bf71285adf92d6586fdf9c757bfb680a2e3
Ahmed Molahez <Ahmed Molahez@DESKTOP-779V4TI>
2018-05-04 15:21:12 +0200

    dsd

--- DS_project/src/eg/edu/alexu/csd/datastructure/mailServer/cs03_cs24_cs54/Mail.java
+++ DS_project/src/eg/edu/alexu/csd/datastructure/mailServer/cs03_cs24_cs54/Mail.java
@@ -33,7 +33,104 @@ public class Mail implements IMail {
 		Mail.email_body = email_body;
 	}
 
-	
+	@SuppressWarnings({ "unchecked", "unused" })
+	@Override
+	public void save_email(String to, String from, String subject, String body) {
+		DLinkedList send = new DLinkedList();
+		DLinkedList recieve = new DLinkedList();
+		DLinkedList subjects = new DLinkedList();
+		DLinkedList bodies = new DLinkedList();
+		DLinkedList orders = new DLinkedList();
+		JSONParser parser = new JSONParser();
+
+		try {
+
+			Object obj = parser.parse(new FileReader("recources/index.json"));
+
+			JSONObject jsonObject = (JSONObject) obj;
+
+			// loop array
+			// here we load content of json file
+			JSONArray col1 = (JSONArray) jsonObject.get("tos");
+			JSONArray col2 = (JSONArray) jsonObject.get("froms");
+			JSONArray col3 = (JSONArray) jsonObject.get("subjects");
+			JSONArray col4 = (JSONArray) jsonObject.get("bodies");
+			JSONArray col5 = (JSONArray) jsonObject.get("order");
+			Iterator<String> iterator1 = col1.iterator();
+			Iterator<String> iterator2 = col2.iterator();
+			Iterator<String> iterator3 = col3.iterator();
+			Iterator<String> iterator4 = col4.iterator();
+			Iterator<String> iterator5 = col5.iterator();
+			while (iterator1.hasNext()) {
+
+				recieve.add(iterator1.next());
+
+			}
+			while (iterator2.hasNext()) {
+				send.add(iterator2.next());
+
+			}
+			while (iterator3.hasNext()) {
+				subjects.add(iterator3.next());
+
+			}
+			while (iterator4.hasNext()) {
+				bodies.add(iterator4.next());
+
+			}
+			while (iterator4.hasNext()) {
+				orders.add(iterator5.next());
+
+			}
+
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		JSONObject obj1 = new JSONObject();
+		JSONArray k1 = new JSONArray();
+		JSONArray k2 = new JSONArray();
+		JSONArray k3 = new JSONArray();
+		JSONArray k4 = new JSONArray();
+		JSONArray k5 = new JSONArray();
+		recieve.add(to);
+		send.add(from);
+		subjects.add(subject);
+		bodies.add(body);
+		if(orders.size()==0) {
+			orders.add("1");
+		}
+		else {
+			orders.add("orders.size() + 1");
+		}
+		for (int i = 0; i < recieve.size(); i++) {
+			k1.add(recieve.get(i));
+			k2.add(send.get(i));
+			k3.add(subjects.get(i));
+			k4.add(bodies.get(i));
+			k4.add(orders.get(i));
+		}
+
+		obj1.put("tos", k1);
+		obj1.put("froms", k2);
+		obj1.put("subjects", k3);
+		obj1.put("bodies", k4);
+		obj1.put("order", k5);
+		try (FileWriter file = new FileWriter("Users/contact.json")) {
+
+			file.write(obj1.toString());
+			file.flush();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 		
 		
 		
